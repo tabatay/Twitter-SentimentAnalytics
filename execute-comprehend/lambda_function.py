a48425c7008d63c01bd356a9cdcf9b49168c3ea4
EC2 Default User <ec2-user@ip-172-31-35-90.ap-northeast-1.compute.internal>
2021-01-11 14:32:38 +0000

    modified get-coronavirus-tweet and execute-comprehend

--- execute-comprehend/lambda_function.py
+++ execute-comprehend/lambda_function.py
@@ -1,47 +1,56 @@
 import boto3
+from datetime import datetime, timedelta, timezone
+from decimal import Decimal, ROUND_HALF_UP
+
+import settings
+
+JST = timezone(timedelta(hours=+9), 'JST')
+
+comprehend = boto3.client('comprehend')
+s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     
-    comprehend = boto3.client("comprehend")
-    s3_client = boto3.client('s3')
-    
     s3_event_uri = event['Records'][0]['s3']['object']['key']
-    receive_bucket_name = 'cn2020-s3-raw'
+    print('格納されたS3ファイルのURI:s3://' + settings.receive_bucket_name + str(s3_event_uri))
     
-    print(s3_event_uri)
+    # ファイルの中身を行ごとに読込
+    s3_object = s3_client.get_object(Bucket=settings.receive_bucket_name, Key=s3_event_uri)
+    s3_object_body_lines = s3_object['Body'].read().decode('utf-8').splitlines()
+
+    # ファイルのヘッダ
+    result_str = 'Sentiment,Positive,Negative,Neutral,Mixed\n'
     
-    response = s3_client.get_object(Bucket=receive_bucket_name, Key=s3_event_uri)
-    lines = response['Body'].read().decode('utf-8').splitlines()
-    
-    print(response['Body'].read().splitlines())
-    print(lines)
-    
-    result_str = ''
-    
-    for text in lines:
-        comprehend_result = comprehend.detect_sentiment(Text=text, LanguageCode="ja")
-        print(comprehend_result)
+    # 分析結果をカンマ区切りで行ごとに書き込むための加工
+    i = 1
+    for text in s3_object_body_lines:
         
-        result_str += str(comprehend_result) + '¥r¥n'
-    
-    s3_put_uri = 'comprehend-executed-coronavirus-result/'
-    
-    put_bucket_name = 'cn2020-s3-lake'
-    
-    put_object = s3_client.put_object(Body=result_str,Bucket=put_bucket_name, Key=s3_put_uri)
-    
-    # start_response = comprehend.start_dominant_language_detection_job(
-    #     InputDataConfig={
-    #         'S3Uri': 's3://cn2020-s3-raw/'+str(s3_uri),
-    #         'InputFormat': 'ONE_DOC_PER_FILE'
-    #     },
+        # comprehendの機能でツイートのPositive,Negative,Neutral,Mixed度を分析
+        executed_comprehend_result = comprehend.detect_sentiment(Text=text, LanguageCode='ja')
         
-    #     OutputDataConfig={
-    #         'S3Uri': 's3://cn2020-s3-lake/comprehend-executed-coronavirus-result/'
-    #     },
+        print(str(i)+'行目'+str(executed_comprehend_result))
+        i += 1
         
-    #     DataAccessRoleArn='arn:aws:iam::340759393322:role/tabata-role',
-    #     JobName='coronavirus-tweets'
-    # )
+        tmp_detect_sentiment_str = ''
+
+        # Positive,Negative,Neutral,Mixed度は小数第4位まで
+        tmp_detect_sentiment_str += str(executed_comprehend_result['Sentiment']) + ','
+        tmp_detect_sentiment_str += str(Decimal(str(executed_comprehend_result['SentimentScore']['Positive'])).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)) + ','
+        tmp_detect_sentiment_str += str(Decimal(str(executed_comprehend_result['SentimentScore']['Negative'])).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)) + ','
+        tmp_detect_sentiment_str += str(Decimal(str(executed_comprehend_result['SentimentScore']['Neutral'])).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)) + ','
+        tmp_detect_sentiment_str += str(Decimal(str(executed_comprehend_result['SentimentScore']['Mixed'])).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)) + ','
+        tmp_detect_sentiment_str += str(Decimal(str(executed_comprehend_result['SentimentScore']['Mixed'])).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP))
+        
+        result_str += tmp_detect_sentiment_str + '\n'
+    
+    # 結果格納先パスの宣言
+    s3_put_folder = 'comprehend-executed-coronavirus-result/'
+    s3_put_filename = 'comprehend-coronavirus-result-' + str(datetime.now(JST).strftime('%Y%m%d%H%M%S')) + '.csv'
+    s3_put_uri = s3_put_folder + s3_put_filename
+
+    # 結果を格納
+    put_object = s3_client.put_object(Body=result_str, Bucket=settings.put_bucket_name, Key=s3_put_uri)
+    
+    print(put_object)
     
     return 0
--- get-coronavirus-tweet/lambda_function.py
+++ get-coronavirus-tweet/lambda_function.py
@@ -7,106 +7,103 @@ import hashlib
 
 import settings
 
-def search_tweets(CK, CKS, AT, ATS, word, count, range):
+kinesis = boto3.client('kinesis', region_name='ap-northeast-1')
+
+def search_tweets(CK, CKS, AT, ATS, search_keyword, count, range):
     
-    api_head = 'https://api.twitter.com/1.1/search/tweets.json?lang=ja&q='
+    twitter_api_head = 'https://api.twitter.com/1.1/search/tweets.json?lang=ja&q='
     
-    # 文字列設定
-    word += ' exclude:retweets' # RTは除く
-    word = urllib.parse.quote_plus(word)
+    # 検索結果のうちリツイートを除外
+    search_keyword += ' exclude:retweets'
+    # URLエンコード
+    search_keyword = urllib.parse.quote_plus(search_keyword) 
     
-    # リクエスト
-    url = api_head+word+"&count="+str(count)
+    # リクエストURL
+    twitter_url = twitter_api_head+search_keyword+'&count='+str(count)+'&tweet_mode=extended'
+    
+    # OAuth1.0での認証
     auth = OAuth1(CK, CKS, AT, ATS)
-    response = requests.get(url, auth=auth)
-    data = response.json()['statuses']
+    
+    # 検索数制限越えの確認
+    try:
+        response = requests.get(twitter_url, auth=auth)
+        search_data = response.json()['statuses']
+    except Exception as e:
+        print(e)
+    
+    print('検索結果:'+str(search_data))
     
     # 2回目以降のリクエスト
-    cnt = 0
+    count = 0
     tweets = []
     while True:
-        if len(data) == 0:
+        # 検索結果が存在するか
+        if len(search_data) == 0:
             break
-        cnt += 1
-        if cnt > range:
+        
+        count += 1
+        
+        # 指定回以上リクエストしたか
+        if count > range:
             break
-        for tweet in data:
-            tweets.append(tweet['text'])
-            maxid = int(tweet["id"]) - 1
-        print("test")
-        url = api_head+word+"&count="+str(count)+"&max_id="+str(maxid)
-        response = requests.get(url, auth=auth)
+        
+        print('検索結果:'+str(search_data))
+        
+        for tweet in search_data:
+            tweets.append(tweet['full_text'])
+            max_id = int(tweet['id']) - 1
+        
+        twitter_url = twitter_api_head+search_keyword+'&count='+str(count)+'&max_id='+str(max_id)+'&tweet_mode=extended'
+        
+        # 検索数制限越えの確認
         try:
-            data = response.json()['statuses']
-        except KeyError: # リクエスト回数が上限に達した場合のデータのエラー処理
-            print('上限エラー')
+            response = requests.get(twitter_url, auth=auth)
+            search_data = response.json()['statuses']
+        except Exception as e:
+            print(e)
             break
+    
     return tweets
     
 def lambda_handler(event, context):
     
     [CK,CKS,AT,ATS] = [settings.CK,settings.CKS,settings.AT,settings.ATS]
+    
+    search_keyword = settings.search_keyword
+    print('検索キーワード:'+str(search_keyword))
 
-    # 検索時のパラメーター
-    word = 'コロナ 旅行' 
-    # word = sys.argv[1] # 検索ワード
-    print(word)
-    count = 10 # 一回あたりの検索数(最大100/デフォルトは15)
-    range = 5 # 検索回数の上限値(最大180/15分でリセット)
+    # 1回あたりのツイート検索件数(最大100, デフォルトは15件)
+    count = settings.count 
+    # 検索回数の上限値(最大180, 15分でリセット)
+    range = settings.range 
     
     # ツイート検索・テキストの抽出
-    tweets = search_tweets(CK, CKS, AT, ATS, word, count, range)
-    # 検索結果を表示
-    # print("tweets:"+str(tweets))
-    
-    # kinesisにin
-    stream_name="coronavirus-tweet-stream"
-    kinesis = boto3.client("kinesis", region_name="ap-northeast-1")
-    word_utf8 = word.encode('utf-8')
-    
-    # partition_keyをハッシュ化
-    partition_key = hashlib.md5(word_utf8).hexdigest()
-    
+    tweets = search_tweets(CK, CKS, AT, ATS, search_keyword, count, range)
     
     tweets_preprocessed = []
     for tweet in tweets:
-        tweet_newline_deleted = tweet.replace('\n','') # ツイート内の改行を削除
-        tweet_newline_deleted += '\n' # ツイート末尾で改行
+        # ツイート内に存在する改行を削除
+        tweet_newline_deleted = tweet.replace('\n','') 
+        # ツイート末尾で改行
+        tweet_newline_deleted += '\n' 
+        
         tweets_preprocessed.append(tweet_newline_deleted)
         
-    print("tweets_newline_deleted:"+str(tweets_preprocessed))
+    print('tweets_preprocessed:'+str(tweets_preprocessed))
     
+    # Kinesis設定
+    stream_name = settings.stream_name
+    search_keyword_utf8 = search_keyword.encode('utf-8')
+    
+    # partition_keyは検索キーワードでハッシュ化したものを指定
+    partition_key = hashlib.md5(search_keyword_utf8).hexdigest()
+    
+    # 1件ずつKinesisDataStreamにput
     for tweet in tweets_preprocessed:
         response = kinesis.put_record(
             Data=tweet,
             PartitionKey=partition_key,
             StreamName=stream_name
         )
-    print("respose:"+str(response))
-    
-    #print(tweets[0])
-    #kinesis.put_record(StreamName=stream_name,Data=tweets[0],PartitionKey=partition_key)
-    
-    # 確認
-    '''
-    response = client.get_shard_iterator(
-        StreamName=stream_name,
-        ShardId='',
-        ShardIteratorType='AT_SEQUENCE_NUMBER'|'AFTER_SEQUENCE_NUMBER'|'TRIM_HORIZON'|'LATEST'|'AT_TIMESTAMP',
-        StartingSequenceNumber='string',
-        Timestamp=datetime(2015, 1, 1)
-    )
-    shards = stream['StreamDescription']['Shards'][0]['ShardId']
-    kinesis_iterator = kinesis.get_shard_iterator(stream_name,shards,'LATEST') 
- 
-    next_iterator = None
-    while True:
-        if next_iterator is None:
-            next_iterator = kinesis_iterator['ShardIterator']
-        else:
-            next_iterator = responce['NextShardIterator']
-     
-        responce = None
-        responce = connection.get_records(next_iterator,limit=1)
-        print(responce['Records'])
-    '''
+
+    print('kinesis.put_record respose:'+str(response))
